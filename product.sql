fb347fdbdb3e3f639782d276e382b12c62a364dc
faridlan <faridlannulhakim@gmail.com>
2023-01-24 23:19:50 +0700

    add syntax for mysql db

---
+++ product.sql
@@ -0,0 +1,59 @@
+-- Active: 1674574060255@@127.0.0.1@3306@nostra
+
+CREATE Table
+    product (
+        id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        price INT NOT NULL,
+        quantity INT NOT NULL DEFAULT 0,
+        created_at BIGINT NOT NULL,
+        updated_at BIGINT NOT NULL
+    ) engine = InnoDB;
+
+show tables;
+
+ALTER Table product MODIFY updated_at BIGINT NULL;
+
+DESC product;
+
+INSERT INTO
+    product(
+        name,
+        price,
+        quantity,
+        created_at
+    )
+VALUES (
+        "Laptop Lenovo",
+        9000000,
+        10,
+        1674574399076
+    ), (
+        "Laptop Asus",
+        8000000,
+        11,
+        1674574399076
+    );
+
+SELECT * FROM product;
+
+INSERT INTO
+    product(
+        name,
+        price,
+        quantity,
+        created_at
+    )
+VALUES (
+        "Laptop Lenovo",
+        9000000,
+        10,
+        1674574399076
+    ), (
+        "Laptop Asus",
+        8000000,
+        11,
+        1674574399076
+    );
+
+UPDATE product SET updated_at = 1674576973 WHERE id = 3 ;
