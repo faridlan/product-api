cae65939f7e95c68a908cd8d7ce2efd8308f47ae
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2023-02-07 23:27:28 +0700

    add unit testing for service

--- test/service/user_service_test.go
+++ test/service/user_service_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/faridlan/product-api/helper"
+	"github.com/faridlan/product-api/helper/mysql"
 	"github.com/faridlan/product-api/model/domain"
 	"github.com/faridlan/product-api/model/web"
 	"github.com/faridlan/product-api/repository"
@@ -199,4 +200,104 @@ func TestCreate(t *testing.T) {
 
 	assert.Equal(t, productExpec, productResponse)
 
+}
+
+func TestUpdate(t *testing.T) {
+
+	db, mock, err := sqlMock()
+	helper.PanicIfErr(err)
+	validate := validator.New()
+
+	defer db.Close()
+
+	ProductService := NewService(db, validate)
+
+	productUpdate := web.ProductUpdate{
+		Id:        1,
+		Name:      "Laptop Lenovo Core i3",
+		Price:     8000000,
+		Quantity:  15,
+		UpdatedAt: time.Now().UnixMilli(),
+	}
+
+	product1 := domain.Product{
+		Id:        1,
+		Name:      "Laptop Asus",
+		Price:     6000000,
+		Quantity:  10,
+		CreatedAt: time.Now().UnixMilli(),
+		// UpdatedAt: &mysql.NullInt{},
+	}
+
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT id, name, price, quantity, created_at, updated_at FROM product WHERE id = ?").WithArgs(1).WillReturnRows(sqlmock.NewRows(
+		[]string{
+			"id",
+			"name",
+			"price",
+			"quantity",
+			"created_at",
+			"updated_at",
+		},
+	).AddRow(product1.Id, product1.Name, product1.Price, product1.Quantity, product1.CreatedAt, product1.UpdatedAt))
+	mock.ExpectExec("UPDATE product SET name = ?, price = ?, quantity = ?, updated_at = ? where id = ?").WithArgs(productUpdate.Name, productUpdate.Price, productUpdate.Quantity, productUpdate.UpdatedAt, productUpdate.Id).WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	product := ProductService.Update(context.Background(), productUpdate)
+
+	productExpec := web.ProductResponse{
+		Id:        1,
+		Name:      productUpdate.Name,
+		Price:     productUpdate.Price,
+		Quantity:  productUpdate.Quantity,
+		CreatedAt: product1.CreatedAt,
+		UpdatedAt: &mysql.NullInt{
+			NullInt64: sql.NullInt64{
+				Int64: productUpdate.UpdatedAt,
+				Valid: true,
+			},
+		},
+	}
+
+	assert.Equal(t, productExpec, product)
+
+}
+
+func TestDelete(t *testing.T) {
+
+	db, mock, err := sqlMock()
+	helper.PanicIfErr(err)
+	validate := validator.New()
+	defer db.Close()
+
+	ProductService := NewService(db, validate)
+
+	product := domain.Product{
+		Id: 1,
+	}
+
+	product1 := domain.Product{
+		Id:        1,
+		Name:      "Laptop Asus",
+		Price:     6000000,
+		Quantity:  10,
+		CreatedAt: time.Now().UnixMilli(),
+		// UpdatedAt: &mysql.NullInt{},
+	}
+
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT id, name, price, quantity, created_at, updated_at FROM product WHERE id = ?").WithArgs(1).WillReturnRows(sqlmock.NewRows(
+		[]string{
+			"id",
+			"name",
+			"price",
+			"quantity",
+			"created_at",
+			"updated_at",
+		},
+	).AddRow(product1.Id, product1.Name, product1.Price, product1.Quantity, product1.CreatedAt, product1.UpdatedAt))
+	mock.ExpectExec("DELETE FROM product WHERE id = ?").WithArgs(product.Id).WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	ProductService.Delete(context.Background(), product.Id)
 }
