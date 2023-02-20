77ef1e54f10a2cbd6db668a0b23a9b46a7aed66e
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2023-02-20 21:12:02 +0700

    add logging

--- helper/logging/product_logger.go
+++ helper/logging/product_logger.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"os"
 
+	"github.com/faridlan/product-api/helper"
 	"github.com/faridlan/product-api/model/web"
 	"github.com/sirupsen/logrus"
 )
@@ -13,6 +15,16 @@ func ProductLogger(webRespone web.WebResponse, writer http.ResponseWriter, reque
 
 	logger := logrus.New()
 	logger.SetFormatter(&logrus.TextFormatter{})
+
+	logger.SetOutput(os.Stdout)
+
+	file, err := os.OpenFile("helper/logging/logger.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	helper.PanicIfErr(err)
+
+	defer file.Close()
+
+	logger.SetOutput(file)
+
 	ip, _, err := net.SplitHostPort(request.RemoteAddr)
 	if err != nil {
 		fmt.Fprintf(writer, "userip: %q is not IP:port", request.RemoteAddr)
@@ -28,8 +40,8 @@ func ProductLogger(webRespone web.WebResponse, writer http.ResponseWriter, reque
 		"user_ip": ip,
 		"code":    webRespone.Code,
 		"status":  webRespone.Status,
-		// "data":    data,
-		"method": request.Method,
+		"data":    webRespone.Data,
+		"method":  request.Method,
 	}).Infof("Product")
 
 }
@@ -38,6 +50,16 @@ func ProductLoggerError(webRespone web.WebResponse, writer http.ResponseWriter, 
 
 	logger := logrus.New()
 	logger.SetFormatter(&logrus.TextFormatter{})
+
+	logger.SetOutput(os.Stdout)
+
+	file, err := os.OpenFile("helper/logging/logger.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	helper.PanicIfErr(err)
+
+	defer file.Close()
+
+	logger.SetOutput(file)
+
 	ip, _, err := net.SplitHostPort(request.RemoteAddr)
 	if err != nil {
 		fmt.Fprintf(writer, "userip: %q is not IP:port", request.RemoteAddr)
@@ -50,10 +72,9 @@ func ProductLoggerError(webRespone web.WebResponse, writer http.ResponseWriter, 
 	}
 
 	logger.WithFields(logrus.Fields{
-		"user_ip": ip,
-		"code":    webRespone.Code,
-		"status":  webRespone.Status,
-		// "data":    data,
+		"user_ip":       ip,
+		"code":          webRespone.Code,
+		"status":        webRespone.Status,
 		"method":        request.Method,
 		"error_message": errMessage,
 	}).Error("Product")
