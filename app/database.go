37d059e8b49f77d318e0fb49d0ade79c51185525
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2023-03-06 21:21:36 +0700

    add new env variable

--- app/database.go
+++ app/database.go
@@ -13,8 +13,9 @@ func NewDatabase() *sql.DB {
 
 	port := os.Getenv("PORT_DB")
 	host := os.Getenv("HOST_DB")
+	name := os.Getenv("NAME_DB")
 
-	db, err := sql.Open("mysql", fmt.Sprintf("root:root@tcp(%s:%s)/nostra", host, port))
+	db, err := sql.Open("mysql", fmt.Sprintf("root:root@tcp(%s:%s)/%s", host, port, name))
 	helper.PanicIfErr(err)
 
 	db.SetConnMaxIdleTime(time.Minute * 10)
